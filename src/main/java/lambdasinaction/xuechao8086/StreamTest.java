14f62880f9b415264270b7d30b526c7199ef2b69
xuechao8086 <xuechao8086@126.com>
2018-08-16 19:09:07 +0800

    并发测试

--- src/main/java/lambdasinaction/xuechao8086/StreamTest.java
+++ src/main/java/lambdasinaction/xuechao8086/StreamTest.java
@@ -1,11 +1,15 @@
 package lambdasinaction.xuechao8086;
 
+import com.google.common.collect.ImmutableList;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
+import javax.annotation.Resource;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 import java.util.function.*;
 import java.util.stream.*;
 
@@ -29,26 +33,23 @@ public class StreamTest {
         });
     }
 
-
     private void testFunc2() {
         // 问题： stream 是怎么split的？
         //       想办法详细debug下？
         Stream.iterate(0, n -> n + 1).limit(10).forEach(System.out::println);
     }
 
-    @Test
     public void testFunc3() {
         Supplier<String> supplier = () -> {
-            int index = (int)Math.round(Math.random())% names.length;
+            int index = (int)Math.round(Math.random()) % names.length;
             return names[index];
         };
 
         Stream.generate(supplier).limit(10).forEach(System.out::println);
     }
 
-
     private void testFunc4() {
-        List<String> result  = Stream.of("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
+        List<String> result = Stream.of("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
             .filter(e -> e.length() > 3)
             .peek(e -> System.out.println("Filtered value: " + e))
             .map(String::toUpperCase)
@@ -59,7 +60,6 @@ public class StreamTest {
 
         result.forEach(System.out::println);
     }
-
 
     /**
      * 一个完整的例子，测试流api如何工作的
@@ -91,24 +91,23 @@ public class StreamTest {
             }
 
             @Override
-            public Set<Characteristics> characteristics(){
-                return Collections.unmodifiableSet(EnumSet.of(Characteristics.IDENTITY_FINISH, Characteristics.CONCURRENT));
+            public Set<Characteristics> characteristics() {
+                return Collections.unmodifiableSet(
+                    EnumSet.of(Characteristics.IDENTITY_FINISH, Characteristics.CONCURRENT));
             }
         }
 
         Supplier<String> supplier = () -> {
-            int index = (int)(System.currentTimeMillis()%names.length);
+            int index = (int)(System.currentTimeMillis() % names.length);
             return names[index];
         };
 
-
-
-        List<String> list = IntStream.range(1, 100_000).mapToObj(String::valueOf).parallel().collect(Collectors.toList());
+        List<String> list = IntStream.range(1, 100_000).mapToObj(String::valueOf).parallel().collect(
+            Collectors.toList());
         List<String> result = list.parallelStream().collect(new ToListCollector<>());
 
         assert result.size() > 0;
     }
-
 
     @Test
     public void testFunc6() {
@@ -122,10 +121,10 @@ public class StreamTest {
             }
 
             public WordCounter accumulate(Character c) {
-                if(Character.isWhitespace(c)) {
-                    return lastSpace? this : new WordCounter(counter, true);
+                if (Character.isWhitespace(c)) {
+                    return lastSpace ? this : new WordCounter(counter, true);
                 } else {
-                    return lastSpace? new WordCounter(counter + 1, false) : this;
+                    return lastSpace ? new WordCounter(counter + 1, false) : this;
                 }
             }
 
@@ -137,7 +136,6 @@ public class StreamTest {
                 return counter;
             }
         }
-
 
         class WordCountSpliterator implements Spliterator<Character> {
             private final String string;
@@ -156,12 +154,13 @@ public class StreamTest {
             @Override
             public Spliterator<Character> trySplit() {
                 int currentSize = string.length() - currentChar;
-                if(currentSize < 10) {
+                if (currentSize < 10) {
                     return null;
                 }
-                for(int splitPos = currentSize/2 + currentChar; splitPos < string.length(); splitPos++) {
-                    if(Character.isWhitespace(string.charAt(splitPos))) {
-                        Spliterator<Character> spliterator = new WordCountSpliterator(string.substring(currentChar, splitPos));
+                for (int splitPos = currentSize / 2 + currentChar; splitPos < string.length(); splitPos++) {
+                    if (Character.isWhitespace(string.charAt(splitPos))) {
+                        Spliterator<Character> spliterator = new WordCountSpliterator(
+                            string.substring(currentChar, splitPos));
                         currentChar = splitPos;
 
                         return spliterator;
@@ -181,19 +180,20 @@ public class StreamTest {
             }
         }
 
-        String sentence =   " Nel   mezzo del cammin  di nostra  vita " +
+        String sentence = " Nel   mezzo del cammin  di nostra  vita " +
             "mi  ritrovai in una  selva oscura" +
             " ché la  dritta via era   smarrita ";
         Stream<Character> stream = IntStream.range(0, sentence.length()).mapToObj(sentence::charAt);
-        WordCounter wordCounter = stream.reduce(new WordCounter(0, true), WordCounter::accumulate, WordCounter::combine);
+        WordCounter wordCounter = stream.reduce(new WordCounter(0, true), WordCounter::accumulate,
+            WordCounter::combine);
         System.out.println("Found " + wordCounter.getCounter() + " words");
 
         Spliterator<Character> spliterator = new WordCountSpliterator(sentence);
         Stream<Character> parallelStream = StreamSupport.stream(spliterator, true);
-        WordCounter parallelWordCount = parallelStream.reduce(new WordCounter(0, true), WordCounter::accumulate, WordCounter::combine);
+        WordCounter parallelWordCount = parallelStream.reduce(new WordCounter(0, true), WordCounter::accumulate,
+            WordCounter::combine);
         System.out.println("Found " + parallelWordCount.getCounter() + " words");
     }
-
 
     @Test
     public void testFunc7() {
@@ -206,38 +206,35 @@ public class StreamTest {
         assert stringSet.size() == 4;
     }
 
-
     @Test
     public void testFunc8() {
-        Function<Integer, Integer> f1 = i -> i*2;
-        Function<Integer, Integer> f2 = f1.compose(i -> i +1);
+        Function<Integer, Integer> f1 = i -> i * 2;
+        Function<Integer, Integer> f2 = f1.compose(i -> i + 1);
 
         Integer r = f2.apply(10);
-        assert  r == 22;
+        assert r == 22;
 
     }
-
 
     @Test
     public void testFunc9() {
         Arrays.asList(1, 2, 3, 4, 5, 6, 7, 9, 8, 0, 1)
             .parallelStream()
-            .filter(i -> i%2 == 0)
+            .filter(i -> i % 2 == 0)
             .forEach(System.out::println);
     }
-
 
     @Test
     public void testFunc10() {
         Map<Integer, Integer> r = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
-            .filter(p ->  p > 5)
-            .collect(Collectors.toMap(p -> p, p -> 2*p, (a, b) -> b));
+            .filter(p -> p > 5)
+            .collect(Collectors.toMap(p -> p, p -> 2 * p, (a, b) -> b));
 
         assert r.size() > 0;
     }
 
     @Test
-    public void testFunc11() throws Exception{
+    public void testFunc11() throws Exception {
         String filePath = "/Users/gumi/Downloads/t.txt";
         Map<String, Long> map = Files.lines(Paths.get(filePath))
             .parallel()
@@ -245,9 +242,27 @@ public class StreamTest {
             .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
         map.forEach((k, v) -> System.out.println(v + "\t" + k));
-
     }
 
+    @Test
+    public void testFunc12() {
+        Map<String, String> a = new HashMap<>(10);
+        Map<String, String> b = new HashMap<>(10);
+        a.putAll(b);
+    }
+
+    @Test
+    public void testFunc13() {
+        String a = "abc.";
+        String b = StringUtils.removeEndIgnoreCase(a, ".");
+
+        assert b.length() > 0;
+    }
+
+    @Test
+    public void testBiTree() {
+
+    }
 
     public static void main(String[] args) {
         System.out.println("== main func begin ==");
@@ -279,6 +294,38 @@ public class StreamTest {
         System.out.println("== next test ==");
 
         System.out.println("== main func ends ==");
+    }
+
+    @Resource
+    private Executor generalExecutor;
+
+    @Test
+    public <T> void testExecutor() {
+        List<String> list = ImmutableList.of("xuechao", "zhao");
+
+        List<CompletableFuture<String>> futures = list.stream()
+            .map(item -> CompletableFuture
+                .supplyAsync(() ->
+                    {
+                        //process...
+                        return "XXX";
+                    },
+                    generalExecutor
+                )
+                .exceptionally(
+                    throwable -> {
+                    System.out.println(throwable.getMessage());
+                    return null;
+                }))
+            .filter(t -> t != null)
+            .collect(Collectors.toList());
+        CompletableFuture<Void> allDoneFuture = CompletableFuture.allOf(
+            futures.toArray(new CompletableFuture[futures.size()]));
+        CompletableFuture<List<String>> cf = allDoneFuture.thenApplyAsync(t ->
+            futures.stream().map(CompletableFuture::join).collect(Collectors.toList())
+        );
+        //获取到多线程的多个返回值的集合
+        List<String> reslutList = cf.join();
     }
 
 }
